51a40bb21f26b4e5dc68383682274f91de54543d
Sam McEldowney <68017476+WinsomeCap@users.noreply.github.com>
2025-10-21 19:55:12 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,9 +2,10 @@ function ValidateCreds() {
   var x = document.getElementById("creds");
   if(x[0]=="R1" and x[1]=="1785")
   {
-    alert("Success.")
+    alert("Success.");
+    window.location.assign('gateway.html');
   }
   else{
-    alert("Failure")
+    alert("Failure");
   }
 }
